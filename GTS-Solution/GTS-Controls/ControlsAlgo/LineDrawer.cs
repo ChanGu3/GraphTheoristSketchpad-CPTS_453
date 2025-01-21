ee6330873fe1d7478b2650769b2c96e8a67c7336
ChandlerGu3 <Chandler.Gu3@gmail.com>
2025-01-21 09:57:05 -0800

    instance line drawer

---
+++ GTS-Solution/GTS-Controls/ControlsAlgo/LineDrawer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTS_Controls
+{
+    internal class LineDrawer
+    {
+        private Color color = Color.Black;
+
+        public LineDrawer() 
+        { 
+        }
+
+        public Color Color
+        {
+            get => color;
+            set => color = value;
+        }
+
+
+
+        private void CreateLineFromTwoNodes(NodeControl node1, NodeControl node2, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen penBlack = new Pen(Color.Black, 5);
+
+            g.DrawLine(penBlack, node1.CenterOrigin, node2.CenterOrigin);
+        }
+    }
+}
